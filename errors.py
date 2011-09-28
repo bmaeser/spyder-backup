1c16bc1808db29515738c71fd3b96d7a023736d4
Bernhard Mäser <bernhard.maeser@gmail.com>
2011-09-28 14:05:16 +0200

    refactoring into dir

--- errors.py
+++
@@ -1,2 +0,0 @@
-class MissingParameterError(RuntimeError):
-    pass
--- test.py
+++
@@ -1,6 +0,0 @@
-from backup import Backup, RemoteBackup
-
-b = Backup()
-b.add_target('some', 'asdf')
-b.fetch()
-b.check_config()
